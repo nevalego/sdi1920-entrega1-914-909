ae20854db03077336f8d1f25d5fab8a639b94aab
Nerea Valdés Egocheaga <nerevalego@gmail.com>
2020-03-15 01:12:45 +0100

    Req 13: Añadir publicación Corregido

--- src/main/java/com/uniovi/controllers/InvitationController.java
+++ src/main/java/com/uniovi/controllers/InvitationController.java
@@ -1,8 +1,6 @@
 package com.uniovi.controllers;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.uniovi.entities.Friendship;
@@ -31,12 +28,6 @@ public class InvitationController {
 
 	@Autowired
 	private UsersService usersService;
-
-	@Autowired
-	private SecurityService securityService;
-	
-	@Autowired
-	private RolesService rolesService;
 	
 	@Autowired
 	private InvitationService invitationsService;
--- src/main/java/com/uniovi/controllers/PublicationsController.java
+++ src/main/java/com/uniovi/controllers/PublicationsController.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -43,26 +42,17 @@ public class PublicationsController {
 
 	@RequestMapping(value = "/publication/add")
 	public String getPublication(Model model) {
-		model.addAttribute("usersList", usersService.getUsers());
 		return "publication/add";
 	}
 
 	@RequestMapping(value = "/publication/add", method = RequestMethod.POST)
 	public String setPublication(@ModelAttribute Publication publication) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String email = auth.getName();
+		User activeUser = usersService.getUserByEmail(email);
 		publication.setDate(new Date());
+		publication.setUser(activeUser);
 		publicationsService.addPublication(publication);
-		return "redirect:/publication/list";
-	}
-
-	@RequestMapping("/publication/details/{id}")
-	public String getDetail(Model model, @PathVariable Long id) {
-		model.addAttribute("publication", publicationsService.getPublication(id));
-		return "publication/details";
-	}
-
-	@RequestMapping("/publication/delete/{id}")
-	public String deletePublication(@PathVariable Long id) {
-		publicationsService.deletePublication(id);
 		return "redirect:/publication/list";
 	}
 }
